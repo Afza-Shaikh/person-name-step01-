99a9200e445f551f46d69c002b4950a51d7d6152
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 21:41:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+let message = "Eric";
+console.log(`hello ${message} would you like to some typescript today?`);
---
+++ app.ts
@@ -0,0 +1,2 @@
+let message:string ="Eric";
+console.log(`hello ${message} would you like to some typescript today?`)
